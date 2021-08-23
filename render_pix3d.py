872d5015bd4a42e0e6aa13b23aa1aab90fc913c2
Vadim Kantorov <vadimkantorov@gmail.com>
2021-08-23 13:39:18 +0300

    ...

--- render_pix3d.py
+++ render_pix3d.py
@@ -14,7 +14,7 @@ import argparse
 import bpy
 import mathutils
 
-def set_camera_location(azimuth, elevation, distance, tilt):
+def set_camera_location_rotation(azimuth, elevation, distance, tilt):
     # render z pass ; render a object z pass map by a given camera viewpoints
     # args in degrees/meters (object centered)
     azimuth, elevation, distance, tilt = map(float, [azimuth, elevation, distance, tilt])
@@ -32,25 +32,42 @@ def set_camera_location(azimuth, elevation, distance, tilt):
     z = z + azimuth     #longtitude
     camera.rotation_euler = (x * math.pi / 180, y * math.pi / 180, z * math.pi / 180)
 
-def init_camera_scene_(w=None, h=None, n_samples=None, xyz=(0, 0, 0), rot_vec_rad=(0, 0, 0), name=None, proj_model='PERSP', f=35, sensor_fit='HORIZONTAL', sensor_width=32, sensor_height=18):
-    bpy.ops.object.camera_add()
-    cam = bpy.context.active_object
+def configure_camera(camera_obj, lens):
+    camera_obj.location = (0, 0, 0)
+    camera_obj.rotation_euler = (0, math.pi, 0)
+    camera_obj.data.type = 'PERSP'
+    camera_obj.data.sensor_width = 32
+    camera_obj.data.sensor_height = 18
+    camera_obj.data.sensor_fit = 'HORIZONTAL'
+    camera_obj.data.lens = data['focal_length']
+
+def configure_scene_render(scene_render, resolution_x, resolution_y, tiles, color_mode, color_depth):
+    scene_render.engine = 'CYCLES'
+    scene_render.image_settings.file_format = 'PNG'
+    scene_render.use_overwrite = True
+    scene_render.use_file_extension = True
+    scene_render.resolution_x = resolution_x
+    scene_render.resolution_y = resolution_y
+    scene_render.resolution_percentage = 100
+    scene_render.tile_x = tiles
+    scene_render.tile_y = tiles 
+    scene_render.image_settings.color_mode = color_mode
+    scene_render.image_settings.color_depth = color_depth
     
-    cam.location = xyz
-    cam.rotation_euler = rot_vec_rad
-    cam.data.type = proj_model
-    cam.data.lens = f
-    cam.data.sensor_fit = sensor_fit
-    cam.data.sensor_width = sensor_width
-    cam.data.sensor_height = sensor_height
+def enable_gpu(use_gpu):
+    if use_gpu:
+        #bpy.context.user_preferences.addons['cycles'].preferences.devices[0].use = True
+        #bpy.context.user_preferences.addons['cycles'].preferences.compute_device_type = 'CUDA'
+        bpy.types.CyclesRenderSettings.device = 'GPU'
+        bpy.data.scenes[bpy.context.scene.name].cycles.device = 'GPU'
 
-    scene = bpy.context.scene
-    scene.render.engine = 'CYCLES'
-    cycles = scene.cycles
-
+def init_camera_scene_regular(n_samples = 50):
+    camera_obj = bpy.data.objects['Camera']
+    camera_obj.data.clip_end = 1e10
+    
+    cycles = bpy.context.scene.cycles
     cycles.use_progressive_refine = True
-    if n_samples is not None:
-        cycles.samples = n_samples
+    cycles.samples = n_samples
     cycles.max_bounces = 100
     cycles.min_bounces = 10
     cycles.caustics_reflective = False
@@ -61,68 +78,19 @@ def init_camera_scene_(w=None, h=None, n_samples=None, xyz=(0, 0, 0), rot_vec_ra
     cycles.volume_bounces = 0
     cycles.transparent_min_bounces = 8
     cycles.transparent_max_bounces = 64
-
-    # Avoid grainy renderings (fireflies)
-    world = bpy.data.worlds['World']
-    world.cycles.sample_as_light = True
     cycles.blur_glossy = 5
     cycles.sample_clamp_indirect = 5
-
-    # Ensure no background node
-    world.use_nodes = True
-    try:
-        world.node_tree.nodes.remove(world.node_tree.nodes['Background'])
-    except KeyError:
-        pass
-
-    scene.render.tile_x = 16
-    scene.render.tile_y = 16
-    if w is not None:
-        scene.render.resolution_x = w
-    if h is not None:
-        scene.render.resolution_y = h
-    scene.render.resolution_percentage = 100
-    scene.render.use_file_extension = True
-    scene.render.image_settings.file_format = 'PNG'
-    scene.render.image_settings.color_mode = 'RGBA' # 'BW'
-    scene.render.image_settings.color_depth = '8'
-
-def init_camera_scene(use_gpu = False, resolution_x = 640, resolution_y = 480, clip_start = 0.5, clip_end = 4.0, hilbert_spiral = 512):
-    scene = bpy.data.scenes[bpy.context.scene.name]
-    scene.render.engine = 'CYCLES'
-    scene.render.image_settings.file_format = 'PNG'
-    scene.render.use_overwrite = True
-    scene.render.use_file_extension = True
-    scene.render.resolution_x, scene.render.resolution_y = resolution_x, resolution_y
-    scene.render.resolution_percentage = 100
-
+    
     world = bpy.data.worlds['World']
-    world.light_settings.use_ambient_occlusion = True
-    world.light_settings.ao_factor = 0.9
+    world.cycles.sample_as_light = True
+    world.use_nodes = True
+    world.node_tree.nodes.remove(world.node_tree.nodes['Background']) if 'Background' in world.node_tree.nodes else None
 
-    if use_gpu:
-        scene.render.engine = 'CYCLES'
-        scene.render.tile_x = scene.render.tile_y = hilbert_spiral
-        #bpy.context.user_preferences.addons['cycles'].preferences.devices[0].use = True
-        #bpy.context.user_preferences.addons['cycles'].preferences.compute_device_type = 'CUDA'
-        bpy.types.CyclesRenderSettings.device = 'GPU'
-        scene.cycles.device = 'GPU'
-    
+def init_camera_scene_depth(color_mode, color_depth, clip_start = 0.5, clip_end = 4.0):
     camera = bpy.data.cameras['Camera']
-    #camera.clip_start = clip_start
-    #camera.clip_end = clip_end
-
-    cam = bpy.data.objects['Camera']
+    camera.clip_start = clip_start
+    camera.clip_end = clip_end
     
-    cam.location = (0, 0, 0)
-    cam.rotation_euler = (0, math.pi, 0)
-    cam.rotation_mode = 'XYZ'
-    cam.data.type = 'PERSP'
-    cam.data.lens = 35
-    cam.data.sensor_fit = 'HORIZONTAL'
-    cam.data.sensor_width = 32
-    cam.data.sensor_height = 18
-
     bpy.context.scene.use_nodes = True
     tree = bpy.context.scene.node_tree
     links = tree.links
@@ -131,14 +99,14 @@ def init_camera_scene(use_gpu = False, resolution_x = 640, resolution_y = 480, c
     render_layer_node = tree.nodes.new('CompositorNodeRLayers')
     map_value_node = tree.nodes.new('CompositorNodeMapValue')
     file_output_node = tree.nodes.new('CompositorNodeOutputFile')
-    #map_value_node.offset[0] = -clip_start
-    #map_value_node.size[0] = 1 / (clip_end - clip_start)
-    #map_value_node.use_min = True
-    #map_value_node.use_max = True
-    #map_value_node.min[0] = 0.0
-    #map_value_node.max[0] = 1.0
-    file_output_node.format.color_mode = 'RGBA'
-    file_output_node.format.color_depth = '8'
+    map_value_node.offset[0] = -clip_start
+    map_value_node.size[0] = 1 / (clip_end - clip_start)
+    map_value_node.use_min = True
+    map_value_node.use_max = True
+    map_value_node.min[0] = 0.0
+    map_value_node.max[0] = 1.0
+    file_output_node.format.color_mode = color_mode
+    file_output_node.format.color_depth = color_depth
     file_output_node.format.file_format = 'PNG' 
     links.new(render_layer_node.outputs[2], map_value_node.inputs[0])
     links.new(map_value_node.outputs[0], file_output_node.inputs[0])
@@ -156,40 +124,64 @@ if __name__ == '__main__':
     rot_mat_by_category = json.load(open(args.rot_mat))
     model_paths = sorted(set(m['model'] for m in meta))
     
-    file_output_node = init_camera_scene()
+    data = meta[0]
+    #w, h = data['img_size']
+    #f = data['focal_length']
+    w, h = 640, 480
+    f = 35
+    
+    hilbert_spiral = 512
+    color_mode, color_depth = 'BW', '8'
+    
+    bpy.context.scene.render.engine = 'CYCLES'
+    world = bpy.data.worlds['World']
+    world.light_settings.use_ambient_occlusion = True
+    world.light_settings.ao_factor = 0.9
 
-    m = meta[0]
-    model_paths = [m['model']]
-    trans_vec = m['trans_mat']
-    rot_mat = m['rot_mat']
+    scene = bpy.data.scenes[bpy.context.scene.name]
+    configure_scene_render(scene.render, w, h, hilbert_spiral, color_mode = color_mode, color_depth = color_depth)
+    
+    configure_camera(bpy.data.objects['Camera'], f)
+    bpy.context.scene.camera = bpy.data.objects['Camera']
+    
+    #file_output_node = init_camera_scene_depth(color_mode = color_mode, color_depth = color_depth)
+    init_camera_scene_regular()
+
+    enable_gpu(use_gpu = False)
 
     for i, model_path in enumerate(model_paths):
-        print(i, '/', len(model_paths))
+        print(i, '/', len(model_paths), model_path)
         model_dir = os.path.join(args.output_path, os.path.dirname(model_path))
         category = os.path.basename(os.path.dirname(os.path.dirname(model_path)))
         os.makedirs(model_dir, exist_ok = True)
         rot_mats = rot_mat_by_category[category]
         
+        #model_paths = [data['model']]
+        #trans_vec = data['trans_mat']
+        #rot_mat = data['rot_mat']
+ 
         bpy.ops.object.select_all(action = 'DESELECT')
         for obj in bpy.data.objects:
             if obj.type == 'MESH':
                 obj.select_set(True)
         bpy.ops.object.delete()
-
-        bpy.ops.import_scene.obj(filepath = os.path.join(os.path.dirname(args.input_path), model_path))
+        
+        bpy.ops.import_scene.obj(filepath=os.path.join(os.path.dirname(args.input_path), model_path), axis_forward='-Z', axis_up='Y')
         obj = bpy.context.selected_objects[0]
+        
+        for k, rot_mat in enumerate(rot_mats):
+            frame_path = os.path.join(model_dir, 'view-{:06}.png'.format(1 + k))
+            
+            obj.matrix_world = mathutils.Matrix.Translation(trans_vec) @ mathutils.Matrix(rot_mat).to_4x4()
+            
+            #file_output_node.base_path = os.path.dirname(frame_path)
+            #file_output_node.file_slots[0].path = 'view-######.png'
+            #bpy.ops.render.render(write_still = False)
+    
+            bpy.context.scene.render.filepath = frame_path
+            bpy.ops.render.render(write_still = True)
 
-        #for rot_mat in rot_mats:
-        if True:
-            trans_4x4 = mathutils.Matrix.Translation(trans_vec)
-            rot_4x4 = mathutils.Matrix(rot_mat).to_4x4()
-            obj.matrix_world = trans_4x4 @ rot_4x4
-
-            file_output_node.base_path = model_dir
-            file_output_node.file_slots[0].path = 'view-######.png'
-
-            bpy.ops.render.render(write_still = False)
             bpy.context.scene.frame_set(1 + bpy.context.scene.frame_current)
 
-            break
+            print(frame_path)
         break
