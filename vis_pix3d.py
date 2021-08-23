05ea6663c8284768804c76fdab20921ccf850f01
Vadim Kantorov <vadimkantorov@gmail.com>
2021-08-23 22:51:25 +0300

    ...

--- vis_pix3d.py
+++ vis_pix3d.py
@@ -5,36 +5,35 @@ import argparse
 
 import bpy
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input-path', '-i', default = 'data/common/pix3d/pix3d.json')
-    parser.add_argument('--output-path', '-o', default = 'test.blend')
-    args = parser.parse_args(sys.argv[1 + sys.argv.index('--'):] if '--' in sys.argv else [])
-    
-    meta = json.load(open(args.input_path))
-    key_model = lambda m: m['model']
-    by_model = [list(g) for k, g in itertools.groupby(sorted(meta, key = key_model), key = key_model)]
-    
-    idx = 0
-    views = by_model[idx]
-    model_path = views[0]['model']
+parser = argparse.ArgumentParser()
+parser.add_argument('--input-path', '-i', default = 'data/common/pix3d/pix3d.json')
+parser.add_argument('--output-path', '-o', default = 'test.blend')
+parser.add_argument('--model-idx', type = int, default = 0)
+parser.add_argument('--view-slice', type = int, nargs = '*')
+args = parser.parse_args(sys.argv[1 + sys.argv.index('--'):] if '--' in sys.argv else [])
 
-    bpy.data.objects['Cube'].select_set(True)
-    bpy.data.objects['Camera'].select_set(True)
-    bpy.ops.object.delete()
-    
-    bpy.ops.import_scene.obj(filepath = model_path, axis_forward='-Z', axis_up='Y')
-    obj = bpy.context.selected_objects[0]
-    obj.location = (0, 0, 0)
+meta = json.load(open(args.input_path))
+key_model = lambda m: m['model']
+by_model = [list(g) for k, g in itertools.groupby(sorted(meta, key = key_model), key = key_model)]
+views = by_model[args.model_idx]
+model_path = views[0]['model']
+view_slice = slice(None) if args.view_slice is None else slice(args.view_slice[0], 1 + args.view_slice[0]) if len(args.view_slice) == 1 else slice(*args.view_slice)
 
-    for m in views:
-        bpy.ops.object.camera_add(location = m['cam_position'])
-        print(m['cam_position'])
-        camera_obj = bpy.context.selected_objects[0]
-        camera_obj.data.lens = m['focal_length']
-        #track_to = bpy.context.object.constraints.new('TRACK_TO')
-        #track_to.target = obj
-        #track_to.track_axis = 'TRACK_NEGATIVE_Z'
-        #track_to.up_axis = 'UP_Y'
+bpy.ops.object.select_all(action = 'SELECT')
+bpy.ops.object.delete()
 
-    bpy.ops.wm.save_mainfile(filepath = args.output_path)
+bpy.ops.import_scene.obj(filepath = model_path, axis_forward = 'Y', axis_up = 'Z')
+obj = bpy.context.selected_objects[0]
+obj.location = (0, 0, 0)
+
+for m in views[view_slice]:
+    bpy.ops.object.camera_add(location = m['cam_position'])
+    print(m['cam_position'], m['img'])
+    camera_obj = bpy.context.selected_objects[0]
+    camera_obj.data.lens = m['focal_length'] # 10
+    track_to = bpy.context.object.constraints.new('TRACK_TO')
+    track_to.target = obj
+    track_to.track_axis = 'TRACK_NEGATIVE_Z'
+    track_to.up_axis = 'UP_Y'
+
+bpy.ops.wm.save_mainfile(filepath = args.output_path)
