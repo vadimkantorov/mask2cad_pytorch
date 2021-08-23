b0ae2956c854ce6eb9fea2893f79c4d2e86179f1
Vadim Kantorov <vadimkantorov@gmail.com>
2021-08-23 20:03:45 +0300

    ...

--- vis_pix3d.py
+++ vis_pix3d.py
@@ -25,13 +25,16 @@ if __name__ == '__main__':
     
     bpy.ops.import_scene.obj(filepath = model_path, axis_forward='-Z', axis_up='Y')
     obj = bpy.context.selected_objects[0]
+    obj.location = (0, 0, 0)
 
     for m in views:
-        res2 = bpy.ops.object.camera_add(location = m['cam_position'])
-        track_to = bpy.context.object.constraints.new('TRACK_TO')
-        track_to.target = obj
-        track_to.track_axis = 'TRACK_NEGATIVE_Z'
-        track_to.up_axis = 'UP_Y'
-        
+        bpy.ops.object.camera_add(location = m['cam_position'])
+        print(m['cam_position'])
+        camera_obj = bpy.context.selected_objects[0]
+        camera_obj.data.lens = m['focal_length']
+        #track_to = bpy.context.object.constraints.new('TRACK_TO')
+        #track_to.target = obj
+        #track_to.track_axis = 'TRACK_NEGATIVE_Z'
+        #track_to.up_axis = 'UP_Y'
 
     bpy.ops.wm.save_mainfile(filepath = args.output_path)
